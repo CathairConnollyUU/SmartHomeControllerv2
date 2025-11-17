7b732ad1f2c9a24925caa1431f85f98b32a87ed1
Cathair Connolly <Connolly-C67@ulster.ac.uk>
2025-11-17 16:01:09 +0000

    Build properties withion smartdevice and smart light

--- SmartHomeController v2/SmartLight.cs
+++ SmartHomeController v2/SmartLight.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SmartHomeController
 {
-    public class SmartLight
+    public class SmartLight : SmartDevice
     {
         // Private fields
         private double brightness;
@@ -25,7 +25,11 @@ namespace SmartHomeController
         }
 
         // Constructor
-        
+        public SmartLight(int deviceID, string deviceName, double brightness, string colour, Boolean status) : base(deviceID, deviceName, status)
+        {
+            Brightness = brightness;
+            Colour = colour;
+        }
 
 
         public void SetBrightness(double brightness)
@@ -38,6 +42,12 @@ namespace SmartHomeController
         {
             this.Colour = colour;
             Console.WriteLine($"Colour now set to {Colour}");
+        }
+
+        public override void GetStatus()
+        {
+            base.GetStatus();
+            Console.WriteLine($"Brightness: {Brightness}%, Colour: {Colour}");
         }
        
     }
